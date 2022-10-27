70e3dc8f7531e763af211aad31dda372e6762f41
Russell Vinegar <38586679+rustyjux@users.noreply.github.com>
2022-10-27 14:33:00 -0700

    removed py3 call

--- install.bat
+++ install.bat
@@ -1,7 +1,6 @@
 @echo off
 REM get QGIS python interpreter
 call "%QGIS_PATH%\bin\o4w_env.bat"
-call py3_env
 REM set required and optional qgis settings to install script
 if not exist %appdata%\QGIS\QGIS3\profiles\default\ (
     echo It appears QGIS has not previously run by your profile. Please open and close QGIS to establish a profile.
